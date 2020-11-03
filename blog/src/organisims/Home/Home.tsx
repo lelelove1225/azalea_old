85e1fd9f808b3dec9dd406fdc6ad2052662c723f
lelelove1225 <m_oota9006@icloud.com>
2020-11-03 19:44:27 +0900

    来る日のためのデプロイ用にDockerFileを作り始めた

--- blog/src/organisims/Home/Home.tsx
+++ blog/src/organisims/Home/Home.tsx
@@ -1,9 +1,19 @@
 import React, { FunctionComponent } from "react"
+import "./Home.css";
+import Logo from "../../atoms/Logo/Logo"
+import Lelelogo from "../../images/lelelove_white.png"
+import LelelogoCopy from "../../images/lelelove_copy_white.png"
 
 const Home : FunctionComponent = () =>{
+    const logoStyle = "Home-Message-Image"
+    const logoStyleSub = "Home-Message-Image-Sub"
+    //FIXME
     const result = (
-        <div>
-            ああああああああああああああ
+        <div className={"Home-Background"}>
+            <div className={"Home-Layout"}>
+                 <Logo style={logoStyle} imgSrc={Lelelogo} />
+                 <Logo style={logoStyleSub} imgSrc={LelelogoCopy} />
+            </div>
         </div>
     );
     return result;
