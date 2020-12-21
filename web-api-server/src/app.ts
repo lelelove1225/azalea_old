90156df40e4cc70a3c713d036178e9083a66e3b9
lelelove1225 <m_oota9006@icloud.com>
2020-12-22 01:29:06 +0900

    はじめてのNode+express＋TS

---
+++ web-api-server/src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet"
+
+const app = express();
+app.use(cors());
+app.use(helmet());
+// body-parserはexpress.json()で代替できる
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+// https://stackoverflow.com/questions/54496398/typescript-type-string-undefined-is-not-assignable-to-type-string
+const port:number = parseInt(process.env.PORT!,10) || 3001;
+
+app.listen(port, ()=> {
+
+})
