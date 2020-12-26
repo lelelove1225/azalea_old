a6f2126c758cc1fb42f2704a487c240f3022fd5b
lelelove1225 <m_oota9006@icloud.com>
2020-12-26 15:31:42 +0900

    wip

--- web-api-server/src/get_blog/get_blog.ts
+++ web-api-server/src/get_blog/get_blog.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
+import { response } from "express";
 
 export function get_blogall() :string {
-    let result :string = ""
-    axios.get()
+    let result :string = "";
+    axios.get("/aaaa")
+    .then((response)=>{
+            console.log(response);
+        }
+    )
     return result
 }
